57cd746630b7b7a501a7582c1e36c32278659e7e
kuronekkoz <kuronekko1808@gmail.com>
2023-12-18 19:13:22 +0700

    Khung layout

---
+++ src/pages/Home/index.js
@@ -0,0 +1,7 @@
+import Header from "~/components/Layout/components/Header";
+
+function Home() {
+  return <h2>Home Page </h2>;
+}
+
+export default Home;
---
+++ src/pages/Search/index.js
@@ -0,0 +1,11 @@
+import Header from "~/components/Layout/components/Header";
+
+function Search() {
+  return (
+    <div>
+      <h2>Search </h2>
+    </div>
+  );
+}
+
+export default Search;
